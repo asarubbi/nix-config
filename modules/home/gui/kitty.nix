4bd2e45c96b20c2a2546b934f66b37deab732c18
Alberto D. Sarubbi <asarubbi@gmail.com>
2026-01-06 16:31:10 -0500

    feat(kitty): set zsh as default shell
    - Configure Kitty to use zsh as the default shell in `modules/home/gui/kitty.nix`.

--- modules/home/gui/kitty.nix
+++ modules/home/gui/kitty.nix
@@ -10,6 +10,7 @@
       enable_audio_bell = false;
       update_check_interval = 0;
       macos_option_as_alt = "both";
+      shell = "zsh";
     };
     shellIntegration.enableZshIntegration = true;
   };
