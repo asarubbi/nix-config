6c23efa8106bd2d9c7fc3d89ec9ce168d87b58fd
Alberto D. Sarubbi <asarubbi@gmail.com>
2026-01-06 14:40:13 -0500

    feat: add rofi themes

--- modules/home/i3/rofi.nix
+++ modules/home/i3/rofi.nix
@@ -1,13 +1,20 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }: # Added config to arguments
+
+let
+  dotfilesDir = "${config.home.homeDirectory}/nix-config";
+in
 
 {
   programs.rofi = {
     enable = true;
+    # We don't set a global theme here because we use multiple per-mode themes.
+    # The default config.rasi will be generated, but specific launches will override it.
   };
 
-  # Source all rofi configuration files from the directory
-  xdg.configFile."rofi" = {
-    source = ./rofi-config;
-    recursive = true;
-  };
+  # Direct symlink to allow editing rofi configs in place
+  xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/modules/home/i3/rofi-config";
+
+  # Install custom Rofi themes to ~/.local/share/rofi/themes/
+  home.file.".local/share/rofi/themes/rounded-nord-dark.rasi".source = "${dotfilesDir}/modules/home/i3/rofi-themes/rounded-nord-dark.rasi";
+  home.file.".local/share/rofi/themes/template/rounded-template.rasi".source = "${dotfilesDir}/modules/home/i3/rofi-themes/template/rounded-template.rasi";
 }
